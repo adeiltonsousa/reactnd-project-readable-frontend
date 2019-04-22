d57d8094c6b3190415cc3e55374f56be96c5bf4f
adeiltonsousa <adeiltonsousaxavier@gmail.com>
2019-04-22 10:49:04 -0300

    Alteração do action de Categories

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -3,10 +3,11 @@ import {
 } from '../action/categories'
 
 
-export default function (state = [], action) {
+export default function categories(state = [], action) {
     switch (action.type) {
         case LIST_CATEGORIES:
-            return state.concat([action.categorie])
+            return state.map((cat) => cat.id !== action.id ? cat :
+            Object.assign({}, cat, { complete: !cat.complete }))
         default:
             return state
     }
